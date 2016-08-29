fbee51d8ba1a93d2ad313064be3a43bdc838cb84
Ed Harcourt <ehar@ehar-gpu-project.stlawu.local>
2016-08-29 17:03:30 -0400

    who knows

--- app/src/main/java/com/example/ehar/cs450activitylifecycle/MainActivity.java
+++ app/src/main/java/com/example/ehar/cs450activitylifecycle/MainActivity.java
@@ -23,7 +23,7 @@ public class MainActivity extends Activity {
             @Override
             public void onClick(View view) {
                 Toast.makeText(
-                        getApplicationContext(),
+                        MainActivity.this,   // a context
                         R.string.toast_message,
                         Toast.LENGTH_SHORT).show();
 
